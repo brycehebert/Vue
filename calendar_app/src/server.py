b6c272dda4b8199cd003dfdf634c3b4863a2a4a7
Bryce Hebert <brycehebert@users.noreply.github.com>
2021-10-05 21:30:22 -0500

    Added Comments

--- calendar_app/src/server.py
+++ calendar_app/src/server.py
@@ -3,24 +3,31 @@ from flask_cors import CORS
 from flask_mongoengine import MongoEngine
 from key import mongoURI
 
+# Flask instance
 app = Flask(__name__)
 
+# CORS
 CORS(app)
 
+# MongoDB Config
 app.config['MONGODB_SETTINGS'] = {
     'host': mongoURI()
 }
 
+# MongoEngine Instance
 db = MongoEngine(app)
 
+# Mongo schema
 class Event(db.Document):
     dayId = db.IntField(required = True)
     details = db.StringField(required = True)
 
+# READ - Return all events 
 @app.route('/get_all', methods=["GET"])
 def get_all():
    return (jsonify(Event.objects), 200)
 
+# CREATE - Add new event to DB
 @app.route("/add_one", methods=["POST"])
 def add_one():
     req = request.json
@@ -28,12 +35,14 @@ def add_one():
     newEvent.save()
     return (jsonify(newEvent), 200)
 
+# UPDATE - Edit an event
 @app.route("/update_one", methods=["PUT"])
 def update_one():
     req = request.json
     newEvent = Event.objects.get_or_404(id = req["_id"]["$oid"]).update(details = req["newEventDetails"])
     return (jsonify(newEvent), 200)
 
+# DELETE - Remove an event
 @app.route("/delete_one", methods=["DELETE"])
 def delete_one():
     req = request.json
@@ -41,5 +50,6 @@ def delete_one():
     delEvent.delete()
     return("Deleted", 200)
 
+# Start server
 if __name__ == '__main__':
     app.run(port = 8000, debug = True)
